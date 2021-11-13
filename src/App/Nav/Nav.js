5819c513d341998cd9d688cb164971ba99d4bd4d
PaulTimothyChambers <63275905+PaulTimothyChambers@users.noreply.github.com>
2021-11-12 19:56:46 -0700

    Flesh out nav component

--- src/App/Nav/Nav.js
+++ src/App/Nav/Nav.js
@@ -1,13 +1,41 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
+
+import homeIcon from './nav-assets/home-icon.png';
+import learningIcon from './nav-assets/learning-icon.png';
+import quizIcon from './nav-assets/quiz-icon.png';
+import favouritesIcon from './nav-assets/favourites-icon.png';
 
 const Nav = () => {
 
   return (
-    <div>
-    </div>
+    <section className="nav-tabs">
+      <NavLink to='/' className="navers">
+        <div className="nav-tabs__home">
+          <p className="home__text">Home</p>
+          <img className="home__icon" src={homeIcon} />
+        </div>
+      </NavLink>
+      <NavLink to='/learning_center' className="navers">
+        <div className="nav-tabs__learning-center">
+          <p className="learning-center__text">Learning Center</p>
+          <img className="learning-center__icon" src={learningIcon} />
+        </div>
+      </NavLink>
+      <NavLink to='/quiz_center' className="navers">
+        <div className="nav-tabs__quiz-center">
+          <p className="quiz-center__text">Quiz Center</p>
+          <img className="quiz-center__icon" src={quizIcon} />
+        </div>
+      </NavLink>
+      <NavLink to='/favourites' className="navers">
+        <div className="nav-tabs__favourites">
+          <p className="favourites__text">Favourites</p>
+          <img className="favourites__icon" src={favouritesIcon} />
+        </div>
+      </NavLink>
+    </section>
   )
-
 }
 
 export default Nav;
