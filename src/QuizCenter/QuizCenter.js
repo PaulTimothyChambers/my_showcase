cb98ebeab3073634b1754e300c999c85938b6a90
PaulTimothyChambers <63275905+PaulTimothyChambers@users.noreply.github.com>
2021-11-12 19:57:42 -0700

    Flesh out quizcenter component

--- src/QuizCenter/QuizCenter.js
+++ src/QuizCenter/QuizCenter.js
@@ -1,12 +1,38 @@
-import React from 'react';
-import { Route } from 'react-router-dom';
+import React, { Component } from 'react';
+import { loadQuizQuestions } from '../apiCalls/apiCalls';
+import QuizForm from '../QuizForm/QuizForm'
+// import { Route } from 'react-router-dom';
 
-const QuizCenter = () => {
+class QuizCenter extends Component {
+  state = {
+    quizQuestions: [],
+    error: null
+  }
 
-  return (
-    <div>
-    </div>
-  )
+  componentDidMount() {
+    loadQuizQuestions()
+      .then(data => this.setState({ quizQuestions: data.questionsList }))
+      .catch(error => this.setState({ error: error.message }))
+  }
+
+  render() {
+    return (
+      <>
+        {
+          this.state.quizQuestions.length &&
+            <section className="quiz-center">
+              <h1 className="quiz-center__header-text">The QUIZ CENTER</h1>
+              <h3 className="quiz-center__subtitle">(I'm secretly keeping score... 0_o)</h3>
+              {
+                !this.state.questionOne &&
+                <p className="quiz-center__standby">Click "Get New Questions" (located below) to begin the quiz!</p>
+              }
+              <QuizForm quizQuestions={ this.state.quizQuestions } />
+            </section>
+        }
+      </>
+    )
+  }
 }
 
 export default QuizCenter;
