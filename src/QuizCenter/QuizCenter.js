20c636bf3d1dc351e5eae0f79cd03abfca584a87
PaulTimothyChambers <63275905+PaulTimothyChambers@users.noreply.github.com>
2021-11-14 05:55:51 -0700

    Change state and methods for new functionality structure

--- src/QuizCenter/QuizCenter.js
+++ src/QuizCenter/QuizCenter.js
@@ -1,37 +1,38 @@
 import React, { Component } from 'react';
-import { loadQuizQuestions } from '../apiCalls/apiCalls';
+import { loadQuestions } from '../apiCalls/apiCalls';
 import QuizForm from '../QuizForm/QuizForm'
 
 class QuizCenter extends Component {
   state = {
-    quizQuestions: [],
+    openQuestions: [],
+    closedQuestions: [],
     error: null
   }
 
   componentDidMount() {
-    loadQuizQuestions()
-      .then(data => this.setState({ quizQuestions: data.questionsList }))
+    loadQuestions()
+      .then(data => this.setState({
+        openQuestions: data.questions.openQuestions,
+        closedQuestions: data.questions.closedQuestions
+      }))
       .catch(error => this.setState({ error: error.message }))
   }
 
-  render() {
-    return (
-      <>
-        {
-          this.state.quizQuestions.length &&
-            <section className="quiz-center">
-              <h1 className="quiz-center__header-text">The QUIZ CENTER</h1>
-              <h3 className="quiz-center__subtitle">(I'm secretly keeping score... 0_o)</h3>
-              {
-                !this.state.questionOne &&
+    render() {
+      return (
+        <>
+          {
+            this.state.closedQuestions.length &&
+              <section className="quiz-center">
+                <h1 className="quiz-center__header-text">The QUIZ CENTER</h1>
+                <h3 className="quiz-center__subtitle">(I'm secretly keeping score... 0_o)</h3>
                 <p className="quiz-center__standby">Click "Get New Questions" (located below) to begin the quiz!</p>
-              }
-              <QuizForm quizQuestions={ this.state.quizQuestions } />
-            </section>
-        }
-      </>
-    )
-  }
+                <QuizForm openQuestions={ this.state.openQuestions } closedQuestions={ this.state.closedQuestions }/>
+              </section>
+          }
+        </>
+      )
+    }
 }
 
 export default QuizCenter;
